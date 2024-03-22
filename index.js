4fa9e9db63233d000c53397c75436699dea8afac
Maham Saif <mahamsaif2005@gmail.com>
2024-03-23 04:01:31 +0500

    1-6

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to number Guessing Game");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please Guess a Number between 1-10",
+        message: "Please Guess a Number between 1-6",
     },
 ]);
 console.log(answers);
