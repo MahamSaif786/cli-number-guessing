35cfb61aa1db1f85f094db8fffbbe48840dafad4
Maham Saif <mahamsaif2005@gmail.com>
2024-03-23 03:50:11 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random()*6+1);
+
+
+const answers = await inquirer.prompt([
+   {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please Guess a Number between 1-10",
+   },   
+]);
+
+
+console.log(answers);
+
+if(answers.userGuessedNumber === randomNumber){
+console.log("Congratulation! You guessed right number.");
+}
+else{
+console.log("You guessed wrong number.");
+}
